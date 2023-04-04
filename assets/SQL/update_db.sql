50f12092f82528b7773a45828b2fbdd678278e53
seqeuncefive <sequencefive@users.noreply.github.com>
2023-04-04 15:28:56 +0100

    sql update

--- assets/SQL/update_db.sql
+++ assets/SQL/update_db.sql
@@ -5,6 +5,7 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+DROP DATABASE IF EXISTS `cm007`;
 CREATE DATABASE `cm007` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `cm007`;
 
@@ -28,7 +29,11 @@ INSERT INTO `experiences` (`story_id`, `title`, `location`, `description`, `cate
 (12,	'Dundee Trip',	'Invergodon',	'Dundee is a coastal city on the Firth of Tay estuary in eastern Scotland. Its regenerated waterfront has 2 nautical museums: RRS Discovery, Dundee is a coastal city on the Firth of Tay estuary in eastern Scotland. Its regenerated waterfront has 2 nautical museums: RRS Discovery, ',	'Landscape',	'assets/uploads/logo 150px.png',	'user1',	'2023-03-26'),
 (17,	'Fife Coastal ',	'Fife',	'Explore golden beaches and fishing villages on a self-guided walking holiday along the Fife Coastal Path. The trail spans the industrial landscapes of North Queensferry and wends along the Scottish coastline all the way to Newport-on-Tay.',	'Nightlife',	'assets/uploads/fife.jpg',	'user1',	'2023-03-27'),
 (18,	'Dunfermline',	'Dunfermline',	'Dunfermline is a city in Fife and was once the capital of Scotland!. In May 2022, Dunfermline became Scotland\'s newest city as part of HM The Queen\'s Platinum Jubilee celebrations.  ',	'Club',	'assets/uploads/dunfermline.jpg',	'admin',	'2023-04-02'),
-(20,	'Walking Tour',	'Edinburgh',	'This walking tour of up to three hours can be offered in a number of sites within Edinburgh. The Royal Mile, The New Town, The Museums and Art Galleries, Princess street and Gardens, the list goes on. ',	'Landscape',	'assets/uploads/SD.jpg',	'admin',	'2023-04-01')
+(20,	'Walking Tour',	'Edinburgh',	'This walking tour of up to three hours can be offered in a number of sites within Edinburgh. The Royal Mile, The New Town, The Museums and Art Galleries, Princess street and Gardens, the list goes on. ',	'Landscape',	'assets/uploads/SD.jpg',	'admin',	'2023-04-01'),
+(24,	'Family Holidays',	'Glasgow',	'This walking tour of up to three hours can be offered in a number of sites within Edinburgh. The Royal Mile, The New Town, The Museums and Art Galleries, Princess street and Gardens, the list goes on. ',	'Nature',	'assets/uploads/family-at-national-museum.jpeg',	'Admin',	'2023-04-03'),
+(25,	' Farms Glamping 2',	'Inverness',	'Scotland has some superb options for a family break from holiday parks to resort hotels. Make sure to book a few months in advance for the school holidays. The Museums and Art Galleries,  Princess ',	'Club',	'assets/uploads/glamping-hot-tub.jpeg',	'RGU',	'2023-04-04'),
+(26,	'National Museum 3',	'Dundee',	'Not sure where to go on a family break in Scotland? Check out these itineraries for family trips in the north, east or south west. Not sure where to go on a family break in Scotland? Check.  ',	'Nightlife',	'assets/uploads/dunfermline-skyline.jpg',	'User1',	'2023-04-04'),
+(27,	'2 Dunfermline 31',	'others',	'dngjdknf dnfkjgn dnd fngld nfkngd fngkld fngkld fngkld fngkd nkldf ndflj gndljf gldnfg ndfklg ndlfngdnflgj ndlkfngl dfngldn fgndlf gdlkfng dnflg dlfgnl dfngldnf gklnd lfgnldkfnglkdf ',	'Nightlife',	'assets/uploads/dunfermline.jpg',	'rgu2',	'2023-04-04')
 ON DUPLICATE KEY UPDATE `story_id` = VALUES(`story_id`), `title` = VALUES(`title`), `location` = VALUES(`location`), `description` = VALUES(`description`), `category` = VALUES(`category`), `image` = VALUES(`image`), `userid` = VALUES(`userid`), `date_added` = VALUES(`date_added`);
 
 DROP TABLE IF EXISTS `users`;
@@ -48,7 +53,11 @@ INSERT INTO `users` (`id`, `FirstName`, `LastName`, `phone`, `email`, `hashed_pa
 (3,	'user',	'user',	987654,	'user@example.co',	'$2y$10$4QWA.TpvMAtlc609Y5nIbeEVfCcON8jCTpeH/7BpSw7jxT.AJoMvO',	0),
 (8,	'Admin',	'Example',	98765,	'admin@example.co',	'$2y$10$MfMxVX6jYxKMno1GaLVg6uu/6kiNYtRJJo50TnE4rrASXUc/DfaG6',	1),
 (6,	'retrytuiyuolgkyfdtsraewstyu',	'tfiyguligkyfdtysaerwtsrydtiufygouhi;p',	54678,	'trfyujghf@fghgjg.com',	'$2y$10$HBC8IlANYE4Cgyfi.2T0DuMXHIeiTGixifGj782XOx6XPqx.RFgzC',	0),
-(7,	'User1',	'User1',	786586970,	'user@user.co',	'$2y$10$iogXadfnSEIMIfSvDgejTuGG7vE23Ji3GilI2qfxilmGtLoAg.62G',	0)
+(7,	'User1',	'User1',	786586970,	'user@user.co',	'$2y$10$iogXadfnSEIMIfSvDgejTuGG7vE23Ji3GilI2qfxilmGtLoAg.62G',	0),
+(10,	'RGU',	'SOC',	98745684,	'rgu@rgu.co',	'$2y$10$RvRqXAKDaLOn.jPk3E4D4utHhhDUy5BUs0V/ENuMyBlMSrFCkSRuq',	0),
+(15,	'Admin2',	'newAdmin',	8979338,	'admin2@example.co',	'$2y$10$JppOeqWsDLTtUuVMlYCaW.tbxGwX4g4yCONSxRNON1fiCii29dfC6',	1),
+(16,	'rgu2',	'new rgu',	93874589,	'rgu2@rgu.co',	'$2y$10$8yxJRGvPAmZD8ptJb0Kxl.UTzHWvf8YmJAqR5zPvezpjdQoHM5LiG',	0),
+(17,	'ADmin3',	'admin3',	93345,	'admin3@example.co',	'$2y$10$YXueQrzptxsgScq06RZNI.Qa4ilq.SB43O/A4jbkHQ7JwUYpn.8W6',	1)
 ON DUPLICATE KEY UPDATE `id` = VALUES(`id`), `FirstName` = VALUES(`FirstName`), `LastName` = VALUES(`LastName`), `phone` = VALUES(`phone`), `email` = VALUES(`email`), `hashed_password` = VALUES(`hashed_password`), `is_admin` = VALUES(`is_admin`);
 
--- 2023-04-02 20:41:17
+-- 2023-04-04 14:27:39
